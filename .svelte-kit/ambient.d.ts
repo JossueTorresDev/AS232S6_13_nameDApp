85b83f6b2bf8566491538b33d0e9dc5abcec31a4
JossueTorresDev <jheferson.torres.h@vallegrande.edu.pe>
2026-06-12 00:05:45 -0500

    Faucet update

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -40,8 +40,6 @@
 declare module '$env/static/private' {
 	export const ALLUSERSPROFILE: string;
 	export const APPDATA: string;
-	export const AWS_PAGER: string;
-	export const BAT_PAGER: string;
 	export const CHROME_CRASHPAD_PIPE_NAME: string;
 	export const COLOR: string;
 	export const COLORTERM: string;
@@ -49,7 +47,6 @@ declare module '$env/static/private' {
 	export const CommonProgramW6432: string;
 	export const COMPUTERNAME: string;
 	export const ComSpec: string;
-	export const DELTA_PAGER: string;
 	export const DriverData: string;
 	export const EDITOR: string;
 	export const EFC_8100_1262719628: string;
@@ -59,9 +56,6 @@ declare module '$env/static/private' {
 	export const EFC_8100_3789132940: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
-	export const GH_PAGER: string;
-	export const GIT_ASKPASS: string;
-	export const GIT_PAGER: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
 	export const HOMEPATH: string;
@@ -69,7 +63,6 @@ declare module '$env/static/private' {
 	export const LANG: string;
 	export const LOCALAPPDATA: string;
 	export const LOGONSERVER: string;
-	export const MANPAGER: string;
 	export const NODE: string;
 	export const NODE_ENV: string;
 	export const npm_command: string;
@@ -95,7 +88,6 @@ declare module '$env/static/private' {
 	export const NUMBER_OF_PROCESSORS: string;
 	export const OneDrive: string;
 	export const OS: string;
-	export const PAGER: string;
 	export const Path: string;
 	export const PATHEXT: string;
 	export const PROCESSOR_ARCHITECTURE: string;
@@ -106,13 +98,10 @@ declare module '$env/static/private' {
 	export const ProgramFiles: string;
 	export const ProgramW6432: string;
 	export const PROMPT: string;
-	export const PROMPT_COMMAND: string;
 	export const PSModulePath: string;
 	export const PUBLIC: string;
-	export const Q_TERM_DISABLED: string;
 	export const SESSIONNAME: string;
 	export const SystemDrive: string;
-	export const SYSTEMD_PAGER: string;
 	export const SystemRoot: string;
 	export const TEMP: string;
 	export const TERM_PROGRAM: string;
@@ -122,13 +111,8 @@ declare module '$env/static/private' {
 	export const USERDOMAIN_ROAMINGPROFILE: string;
 	export const USERNAME: string;
 	export const USERPROFILE: string;
-	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
-	export const VSCODE_GIT_ASKPASS_MAIN: string;
-	export const VSCODE_GIT_ASKPASS_NODE: string;
-	export const VSCODE_GIT_IPC_HANDLE: string;
 	export const VSCODE_INJECTION: string;
 	export const VSCODE_PYTHON_AUTOACTIVATE_GUARD: string;
-	export const VTE_VERSION: string;
 	export const windir: string;
 }
 
@@ -218,8 +202,6 @@ declare module '$env/dynamic/private' {
 	export const env: {
 		ALLUSERSPROFILE: string;
 		APPDATA: string;
-		AWS_PAGER: string;
-		BAT_PAGER: string;
 		CHROME_CRASHPAD_PIPE_NAME: string;
 		COLOR: string;
 		COLORTERM: string;
@@ -227,7 +209,6 @@ declare module '$env/dynamic/private' {
 		CommonProgramW6432: string;
 		COMPUTERNAME: string;
 		ComSpec: string;
-		DELTA_PAGER: string;
 		DriverData: string;
 		EDITOR: string;
 		EFC_8100_1262719628: string;
@@ -237,9 +218,6 @@ declare module '$env/dynamic/private' {
 		EFC_8100_3789132940: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
-		GH_PAGER: string;
-		GIT_ASKPASS: string;
-		GIT_PAGER: string;
 		HOME: string;
 		HOMEDRIVE: string;
 		HOMEPATH: string;
@@ -247,7 +225,6 @@ declare module '$env/dynamic/private' {
 		LANG: string;
 		LOCALAPPDATA: string;
 		LOGONSERVER: string;
-		MANPAGER: string;
 		NODE: string;
 		NODE_ENV: string;
 		npm_command: string;
@@ -273,7 +250,6 @@ declare module '$env/dynamic/private' {
 		NUMBER_OF_PROCESSORS: string;
 		OneDrive: string;
 		OS: string;
-		PAGER: string;
 		Path: string;
 		PATHEXT: string;
 		PROCESSOR_ARCHITECTURE: string;
@@ -284,13 +260,10 @@ declare module '$env/dynamic/private' {
 		ProgramFiles: string;
 		ProgramW6432: string;
 		PROMPT: string;
-		PROMPT_COMMAND: string;
 		PSModulePath: string;
 		PUBLIC: string;
-		Q_TERM_DISABLED: string;
 		SESSIONNAME: string;
 		SystemDrive: string;
-		SYSTEMD_PAGER: string;
 		SystemRoot: string;
 		TEMP: string;
 		TERM_PROGRAM: string;
@@ -300,13 +273,8 @@ declare module '$env/dynamic/private' {
 		USERDOMAIN_ROAMINGPROFILE: string;
 		USERNAME: string;
 		USERPROFILE: string;
-		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
-		VSCODE_GIT_ASKPASS_MAIN: string;
-		VSCODE_GIT_ASKPASS_NODE: string;
-		VSCODE_GIT_IPC_HANDLE: string;
 		VSCODE_INJECTION: string;
 		VSCODE_PYTHON_AUTOACTIVATE_GUARD: string;
-		VTE_VERSION: string;
 		windir: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
